6da96e5be325b24730f460e1d1bb33ccd2dabd56
Itzel Contreras <itselfcg@gmail.com>
2020-09-13 17:04:52 -0700

    New info section about Baja California and the most important cities form index.php. New stylesheet info.css and fonts.css . Minor changes in navbar for background.

--- index.php
+++ index.php
@@ -6,14 +6,19 @@
     <title>Baja Discover | Home </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/homeStyle.css">
-    <link rel="stylesheet" type="text/css" href="css/navbarStyle.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <script type="text/javascript" src="js/navbar.js"></script>
 
+
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/intro.css">
+    <link rel="stylesheet" type="text/css" href="css/info.css">
+
+    <script type="text/javascript" src="js/navbar.js"></script>
     <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
@@ -58,12 +63,10 @@
                         <a class="dropdown-item" href="#">Tecate</a>
                         <a class="dropdown-item" href="#">Tijuana</a>
                         <a class="dropdown-item" href="#">Rosarito</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Map</a>
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Newsletter</a>
+                    <a class="nav-link" href="#">TOURS</a>
                 </li>
             </ul>
         </div>
@@ -74,7 +77,7 @@
     <section class="intro-section">
         <div id="intro-container">
             <label class="intro-lbl">VISIT BC </label><br>
-            <button class="btn home-btn">MORE INFO</button>
+            <button class="btn home-btn"><a href="#home">MORE INFO</a></button>
         </div>
         <div id="intro-carousel">
             <div id="headerBannerCarousel" class="carousel slide" data-ride="carousel">
@@ -100,9 +103,85 @@
             </div>
         </div>
     </section>
-    <section class="home-section">
+    <section id="home" class="home-section">
 
-        sdfd
+        <div class="home-container">
+            <div class="float-right">
+
+                <img id="map-background" src="imagenes/intro/bajamap.png">
+            </div>
+            <div class="home-info">
+                <h1>BAJA CALIFORNIA</h1>
+                <p class="p1">With over 3 million residents, Baja is the fourteenth most populated state in Mexico. Of
+                    which, Tijuana, claims more than a third of those residents with 1,600,000 people.</p>
+
+                <p class="p2">The state of Baja California is known for many charming small villages. In contrast, are
+                    the large towns of Tijuana, Mexicali, Ensenada, Tecate, and Rosarito. These towns offer many
+                    historical and cultural attractions but are also known for their entertainment venues, restaurants,
+                    and fun..</p>
+
+                <p class="p3">While there are many populated parts of Baja like Tijuana, there are also remote sections
+                    that add to the charm. In contrast, aside from the beach activities along the coast of the state,
+                    there are also eco-tourism opportunities. For example, whale watching, environmental tours, and rock
+                    climbing adventures name a few. In addition, visitors find luxury hotels as well as affordable
+                    travel lodges.</p>
+                <br>
+                <div class="cities-preview">
+                    <div class="row">
+                        <div class="col">
+                            <a href="ensenada">
+                                <div class="preview-container">
+                                    <label class="preview-label">ENSENADA</label>
+                                    <img class="preview-img" src="imagenes/intro/banners/ensenada.jpg">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col">
+                            <a href="mexicali">
+
+                                <div class="preview-container">
+                                    <label class="preview-label">MEXICALI</label>
+                                    <img class="preview-img" src="imagenes/intro/banners/mexicali.jpg">
+                                </div>
+                            </a>
+
+                        </div>
+                        <div class="col">
+                            <a href="rosarito">
+
+                                <div class="preview-container">
+                                    <label class="preview-label">ROSARITO</label>
+                                    <img class="preview-img" src="imagenes/intro/banners/rosarito.jpg">
+                                </div>
+                            </a>
+
+                        </div>
+                        <div class="col">
+                            <a href="tecate">
+
+                                <div class="preview-container">
+                                    <label class="preview-label">TECATE</label>
+                                    <img class="preview-img" src="imagenes/intro/banners/tecate.jpg">
+                                </div>
+                            </a>
+
+                        </div>
+                        <div class="col">
+                            <a href="tijuana">
+
+                                <div class="preview-container">
+                                    <label class="preview-label">TIJUANA</label>
+                                    <img class="preview-img" src="imagenes/intro/banners/tijuana.jpg">
+                                </div>
+                            </a>
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     <section class="info-section">
 
