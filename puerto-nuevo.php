49469111d41642a17a6cb7b8897cda691c90e036
Itzel Contreras <itselfcg@gmail.com>
2020-10-03 17:16:32 -0700

    New puerto nuevo tour page

---
+++ puerto-nuevo.php
@@ -0,0 +1,144 @@
+<?php
+include 'navbar.php';
+include 'libs.php';
+
+?>
+
+    <html>
+    <head>
+        <title>Baja Discover | Tours </title>
+        <link rel="stylesheet" type="text/css" href="css/page-style/puerto-nuevo.css">
+        <link rel="stylesheet" type="text/css" href="css/page-style/pricing-table.css">    </head>
+    <body>
+
+    <main id="puerto-nuevo">
+        <section class="intro">
+            <div class="container">
+                <div class="info">
+
+
+                        <h1>PUERTO NUEVO</h1>
+                        <p style="text-align: center">
+                            <i class="fa fa-clock-o" aria-hidden="true"></i> 2h - 4h&nbsp;&nbsp;&nbsp;
+                            <i class="fa fa-car" aria-hidden="true"></i>Transportation included&nbsp;&nbsp;&nbsp;
+                            <i class="fa fa-users" aria-hidden="true"></i> Private group&nbsp;&nbsp;&nbsp;
+                        </p>
+                        <hr>
+                        <p style="text-align: justify">
+                            Taste the most delicius lobster in México. Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit. Nam a leo eu tortor blandit gravida. Quisque porttitor tellus ex, et
+                            fringilla arcu scelerisque a. Donec volutpat, massa sit amet iaculis volutpat, nisi metus
+                            malesuada metus, ut dapibus quam augue eget tellus. Nullam eu odio nec ipsum gravida
+                            fermentum non et eros. Etiam accumsan nec augue pretium pretium.
+
+
+                        </p>
+
+
+                        <br>
+                        <p class="text-right">
+                            <a href="#pricing" class="btn btn-red"><i class="fa fa-usd" aria-hidden="true"></i>
+
+                                PRICING</a>&nbsp;&nbsp;
+                            <a href="#pricing" class="btn btn-green"><i class="fa fa-address-book"
+                                                                        aria-hidden="true"></i>
+
+
+                                BOOK NOW</a>
+                        </p>
+                    </div>
+
+            </div>
+
+
+        </section>
+
+
+        <section id="pricing" class="pricing-section">
+
+            <div class=" princing-container">
+                <div class="product">
+
+                    <div class="product-title">Basic</div>
+                    <div class="price">$50<sup>/Person</sup></div>
+                    <ul class="table-list">
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Popular restaurant</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Lobster  </li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Transport</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Private guide</li>
+                    </ul>
+                    <div class="product-footer">
+                        <button class="btn btn-gold">Book now</button>
+                    </div>
+                </div>
+                <div class="product">
+                    <div class="product-title">Standar</div>
+                    <div class="price">$120<sup>/ Person</sup></div>
+                    <ul class="table-list">
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Exclusive restaurant</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Lobster  </li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Drinks </li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                             Transport</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Private guide</li>
+
+                    </ul>
+                    <div class="product-footer">
+                        <button class="btn btn-gold">Book now</button>
+                    </div>
+                </div>
+                <div class="product">
+
+                    <div class="product-title">Premium</div>
+                    <div class="price">$180<sup>/Person</sup></div>
+
+                    <ul class="table-list">
+
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Exclusive restaurant</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Lobster  <span class="unlimited">Unlimited</span></li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Drinks  <span class="unlimited">Unlimited</span></li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            VIP Transport</li>
+                        <li><i class="fa fa-check" aria-hidden="true"></i>
+                            Private guide</li>
+
+
+                    </ul>
+                    <div class="footer">
+                        <div class="product-footer">
+                            <button class="btn btn-gold">Book now</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <br><br>
+            <div class="leyend">
+                <p>
+                    Not what you were looking for? <a href="contact.php">Contact us</a> for custom packages
+
+                </p>
+            </div>
+
+        </section>
+    </main>
+    </body>
+
+
+
+<?php
+include 'footer.php';
+
+?>
