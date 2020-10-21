7db2c7accd9cdaf2439e8761dbda40798e8c4506
Itzel Contreras <itselfcg@gmail.com>
2020-10-21 16:58:37 -0700

    Navbar color's change fixes.

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,16 @@
 
+
+
 $(document).ready(function () {
 
 
 
-    $(window).scroll(function () {
+
         var pageHeight = $(window).height();
         changeNavbar(pageHeight);
 
-    });
 
-    $(window).onload(function () {
+    $(window).scroll(function () {
         var pageHeight = $(window).height();
         changeNavbar(pageHeight);
 
@@ -34,13 +35,18 @@ function changeNavbar(height){
         } else {
             $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
             $(".navbar a").css("color", "#FFFFFF");
-
             document.getElementById('media-link').style.display = "block";
+
+            document.getElementById("icon").style.color = "#ffffff";
+            $(".navbar-toggler").css("border-color", "white");
 
         }
     } else {
+        console.log(":D")
         $(".navbar a").css("color", "#000000");
         $(".navbar-fixed-top").css("background-color", "#FFFFFF"); // if not, change it back to transparent
+        document.getElementById("icon").style.color = "#000000";
+        $(".navbar-toggler").css("border-color", "black");
 
         if ($(document).scrollTop() > 50) {
             document.getElementById('media-link').style.display = "none";
